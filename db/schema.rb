62203c319ab8fd37b4ddfb2cff495050f98393ef
chama0 <chama_zer0@yahoo.co.jp>
2021-03-24 12:55:56 +0900

    usersテーブルカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,14 @@
 ActiveRecord::Schema.define(version: 2021_03_17_223550) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "firstname", null: false
+    t.string "lastname", null: false
+    t.string "firstname_reading", null: false
+    t.string "lastname_reading", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
