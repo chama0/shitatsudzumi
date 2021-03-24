c1e5c1bf041a73284bfc782f7a1fcff539e658c5
chama0 <chama_zer0@yahoo.co.jp>
2021-03-24 12:59:04 +0900

    usersテーブルへ保存するカラム値追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :firstname_reading, :lastname_reading, :birthday])
+  end
+end 
