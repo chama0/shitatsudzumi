295e479676d77418a7b160e52a73cd9aa7e82475
chama0 <chama_zer0@yahoo.co.jp>
2021-03-24 14:14:01 +0900

    rubocopによるコード自動修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :firstname_reading, :lastname_reading, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :firstname, :lastname, :firstname_reading, :lastname_reading, :birthday])
   end
-end 
+end
