87bac8ac6de86c6034727fad37ea805c77f22575
chama0 <chama_zer0@yahoo.co.jp>
2021-04-15 07:20:20 +0900

    recipesテーブルへのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,16 @@ ActiveRecord::Schema.define(version: 2021_03_25_054023) do
   end
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "chach_copy", null: false
+    t.text "point", null: false
+    t.text "upbringing", null: false
+    t.integer "many", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,4 +65,5 @@ ActiveRecord::Schema.define(version: 2021_03_25_054023) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "recipes", "users"
 end
