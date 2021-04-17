0c2d6c9c6483ccc3221d9c0896a07ce49b7b0709
chama0 <chama_zer0@yahoo.co.jp>
2021-04-17 10:06:15 +0900

    howtomakesテーブルへのカラム追加

--- db/migrate/20210417004958_create_howtomakes.rb
+++ db/migrate/20210417004958_create_howtomakes.rb
@@ -1,7 +1,9 @@
 class CreateHowtomakes < ActiveRecord::Migration[6.0]
   def change
     create_table :howtomakes do |t|
-
+      t.text       :text,   null: false
+      t.string     :image,  null: false
+      t.references :recipe, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_003355) do
+ActiveRecord::Schema.define(version: 2021_04_17_004958) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_04_17_003355) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "howtomakes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.string "image", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_howtomakes_on_recipe_id"
   end
 
   create_table "materials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -75,6 +84,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_003355) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "howtomakes", "recipes"
   add_foreign_key "materials", "recipes"
   add_foreign_key "recipes", "users"
 end
