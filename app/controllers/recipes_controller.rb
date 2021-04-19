f07c2f458d58d11e64ca1248ea64610b6b58de15
chama0 <chama_zer0@yahoo.co.jp>
2021-04-20 03:14:15 +0900

    newアクション、createアクションのコントローラー設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,32 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :chach_copy, :point, :upbringing, :many, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def material_params(recipe)
+    params.permit(:name, :quantity, :price).merge(recipe_id: recipe.id)
+  end
+
+  def howtomake_params(recipe)
+    params.permit(:text, :image).merge(recipe_id: recipe.id)
   end
 end
