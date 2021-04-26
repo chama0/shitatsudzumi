a22295b5d7eeb7f45f0cb57cce2754d47bc0e282
chama0 <chama_zer0@yahoo.co.jp>
2021-04-26 20:41:20 +0900

    FactoryBot生成

---
+++ spec/factories/recipe_howtomakes.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :recipe_howtomake do
+    name           { Faker::Food.dish }
+    chach_copy     { Faker::Restaurant.type }
+    point          { Faker::Restaurant.description }
+    upbringing     { Faker::Restaurant.review }
+    many           { 2 }
+    price          { 1000 }
+    material_name  { Faker::Food.ingredient }
+    quantity       { Faker::Food.measurement }
+    material_price { 300 }
+    text           { Faker::Food.description }
+    association :user
+    after(:build) do |recipe_howtomake|
+      recipe_howtomake.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
+  end
+end
