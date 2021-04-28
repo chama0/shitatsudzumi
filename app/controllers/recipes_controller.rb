445496117bfec59b13d19d98377cfe72ded4f610
chama0 <chama_zer0@yahoo.co.jp>
2021-04-28 13:37:22 +0900

    indexビューにレシピ一覧を表示するためのコード修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @recipe = Recipe.all.order("created_at DESC")
+    @recipes = Recipe.all.order("created_at DESC")
   end
 
   def new
